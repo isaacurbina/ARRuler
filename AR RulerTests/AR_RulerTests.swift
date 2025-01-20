a41c023aa29126485e02fec0961a3f52a948c59e
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-20 10:44:48 -0600

    Initial Commit

---
+++ AR RulerTests/AR_RulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_RulerTests.swift
+//  AR RulerTests
+//
+//  Created by Isaac Urbina on 1/20/25.
+//
+
+import Testing
+@testable import AR_Ruler
+
+struct AR_RulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
