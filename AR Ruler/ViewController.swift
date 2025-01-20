6e163bc5546f8aceceebc6f4915783dd4976e372
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-20 12:37:55 -0600

    381. Creating 3D Text in the AR Scene (#3)

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -97,9 +97,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 		let a = end.position.x - start.position.x
 		let b = end.position.y - start.position.y
 		let c = end.position.z - start.position.z
-
+		
 		let distance = sqrt(pow(a,2) + pow(b,2) + pow(c,2))
 		
-		print(distance)
+		updateText(text: "\(distance)", at: end.position)
+	}
+	
+	private func updateText(text: String, at position: SCNVector3) {
+		let textGeometry = SCNText(string: text, extrusionDepth: 1)
+		let material = SCNMaterial()
+		material.diffuse.contents = UIColor.red
+		textGeometry.materials = [material]
+		
+		let textNode = SCNNode(geometry: textGeometry)
+		textNode.position = SCNVector3(
+			x: 0,
+			y: 0.01,
+			z: -0.1
+		)
+		textNode.scale = SCNVector3(
+			x: position.x,
+			y: position.y + 0.01,
+			z: position.z
+		)
+		
+		sceneView.scene.rootNode.addChildNode(textNode)
 	}
 }
